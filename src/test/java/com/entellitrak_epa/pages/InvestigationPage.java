7e3bbf76c08e002c3b3eac0e69754230493989d6
Musa <Musa@DESKTOP-6707GA3>
2019-01-23 17:20:47 -0500

    created investigation page and clearance page

---
+++ src/test/java/com/entellitrak_epa/pages/InvestigationPage.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.entellitrak_epa.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.entellitrak_epa.utilities.Driver;
+
+/**
+ * @author Musa
+ *
+ */
+public class InvestigationPage {
+	
+	public InvestigationPage() {
+		PageFactory.initElements(Driver.getInstance(), this);
+	}
+	
+	@FindBy(xpath = "//span[contains(text(), 'Investigation Information')]")
+	public WebElement investigationinformation_section;
+	
+	public WebElement Investigation_investigationType_display;
+	
+	public WebElement Investigation_eqipReleaseToOpmDate;
+	
+	public WebElement Investigation_opmCaseNumber;
+	
+	public WebElement Investigation_investigationScheduledDate;
+	
+	public WebElement Investigation_opmCaseStatus;
+	
+	public WebElement Investigation_investigationClosedDate;
+	
+	public WebElement Investigation_edeliveryMatch_yes;
+	
+	public WebElement Investigation_edeliveryMatch_no;
+	
+	public WebElement Investigation_roiReceivedDate;
+	
+	public WebElement Investigation_reinvestigationDueDate;
+	
+	public WebElement Investigation_investigationComments;
+	
+	public WebElement saveButton;
+	
+	
+	
+	
+	
+
+}
